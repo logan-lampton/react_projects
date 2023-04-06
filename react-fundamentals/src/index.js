b0c066cd90ea79c467832616e0291aa11403884c
Logan Lampton <lampton.logan@gmail.com>
2023-04-06 01:02:58 -0400

    formatted extensions and settings

--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 function Greeting() {
   return <h2>My First Component</h2>;
 }
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<Greeting />);
---
+++ react-fundamentals/src/testing.js
@@ -0,0 +1,11 @@
+// rfce to create a regular function
+
+import React from 'react'
+
+function testing() {
+  return (
+    <div>testing</div>
+  )
+}
+
+export default testing
