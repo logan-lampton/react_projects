44a1dcd13c84a9c93ec645220d8b63dcebe1dd66
Logan Lampton <lampton.logan@gmail.com>
2023-04-07 02:09:43 -0400

    book and booklist styling applied

--- react-fundamentals/src/components/Book.js
+++ react-fundamentals/src/components/Book.js
@@ -1,10 +1,11 @@
 import Author from './Author';
 import Image from './Image';
 import Title from './Title';
+import "../style.css";
 
 function Book() {
   return (
-    <article>
+    <article className="book">
       <Image />
       <Title />
       <Author />
--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Book from './components/Book';
+import './style.css';
 
 function BookList() {
   return (
-    <section>
+    <section className="booklist">
       <Book />
       <Book />
       <Book />
