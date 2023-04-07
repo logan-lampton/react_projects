1b1d0524a0ddd486a2a9bdf397ea4b61474316f6
Logan Lampton <lampton.logan@gmail.com>
2023-04-07 02:25:09 -0400

    added styling to Author component

--- react-fundamentals/src/components/Author.js
+++ react-fundamentals/src/components/Author.js
@@ -1,5 +1,9 @@
 function Author() {
-  return <h4>By Bonnie Garmus</h4>;
+  return (
+    <h4 style={{ color: '#617d98', fontSize: '0.75rem', marginTop: '0.5rem' }}>
+      By Bonnie Garmu
+    </h4>
+  );
 }
 
 export default Author;
