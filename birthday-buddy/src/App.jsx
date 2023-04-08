a429ce75a9f4e0f8dc52419cab7f652fde730d1a
Logan Lampton <lampton.logan@gmail.com>
2023-04-07 20:36:45 -0400

    Your commit message

--- birthday-buddy/src/App.jsx
+++ birthday-buddy/src/App.jsx
@@ -1,10 +1,18 @@
-function App() {
+import { useState } from "react";
+import data from "./data";
 
-  return (
-    <h2>
-      Hey there!
-    </h2>
-  )
+function App() {
+  const [people, setPeople] = useState(data);
+  
+  return <h2>{people.map((person) => {
+    return (
+      <div key={person.id}>
+        <img src={person.image}></img>
+        <div>{person.name}</div>
+        <div>{person.age}</div>
+      </div>
+    )
+  })}</h2>;
 }
 
-export default App
+export default App;
