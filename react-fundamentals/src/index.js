51d45466a19c0aa1360a1007047854543c313cb0
Logan Lampton <lampton.logan@gmail.com>
2023-04-09 04:10:15 -0400

    refactored to one book array of objects; mapped out the array in the BookList component to pass down as props

--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -3,37 +3,39 @@ import ReactDOM from 'react-dom/client';
 import Book from './components/Book';
 import './style.css';
 
-const firstBook = {
-  image: './images/Book1.jpg',
-  title: 'Lessons in Chemistry',
-  author: 'Bonnie Garmus',
-};
-const secondBook = {
-  image: 'https://m.media-amazon.com/images/I/81fyoFoaxlL.jpg',
-  title: 'Dog Man: Twenty Thousand Fleas Under the Sea',
-  author: 'Dav Pilkey',
-};
-const thirdBook = {
-  image:
-    'https://cdn.thebooksiogroup.com/cover_images/642/9781524798642.jpg?width=300&height=446&image-type=product_page_main_image&fit=bounds',
-  title: 'Daisy Jones & The Six',
-  author: 'Taylor Jenkins Reid',
-};
-const fourthBook = {
-  image: 'https://m.media-amazon.com/images/I/91StpJ4e8gL.jpg',
-  title: 'Hello Beautiful',
-  author: 'Ann Napolitano',
-};
+const books = [
+  {
+    image: './images/Book1.jpg',
+    title: 'Lessons in Chemistry',
+    author: 'Bonnie Garmus',
+  },
+  {
+    image: 'https://m.media-amazon.com/images/I/81fyoFoaxlL.jpg',
+    title: 'Dog Man: Twenty Thousand Fleas Under the Sea',
+    author: 'Dav Pilkey',
+  },
+  {
+    image:
+      'https://cdn.thebooksiogroup.com/cover_images/642/9781524798642.jpg?width=300&height=446&image-type=product_page_main_image&fit=bounds',
+    title: 'Daisy Jones & The Six',
+    author: 'Taylor Jenkins Reid',
+  },
+  {
+    image: 'https://m.media-amazon.com/images/I/91StpJ4e8gL.jpg',
+    title: 'Hello Beautiful',
+    author: 'Ann Napolitano',
+  },
+];
 
 function BookList() {
   return (
     <section className='booklist'>
-      <Book {...firstBook}>
-        <button>Buy Now!</button>
-      </Book>
-      <Book {...secondBook} />
-      <Book {...thirdBook} />
-      <Book {...fourthBook} />
+      {books.map((book) => {
+        const {image, title, author} = book;
+        return (
+            <Book image={image} title={title} author={author}/>
+        );
+      })}
     </section>
   );
 }
