ad0f5950ad210b00f721fd7d67a7020a4887a474
Logan Lampton <lampton.logan@gmail.com>
2023-04-10 04:17:06 -0400

    added key prop for Book components in the BookList

--- react-fundamentals/src/index.js
+++ react-fundamentals/src/index.js
@@ -8,22 +8,26 @@ const books = [
     image: './images/Book1.jpg',
     title: 'Lessons in Chemistry',
     author: 'Bonnie Garmus',
+    id: 1
   },
   {
     image: 'https://m.media-amazon.com/images/I/81fyoFoaxlL.jpg',
     title: 'Dog Man: Twenty Thousand Fleas Under the Sea',
     author: 'Dav Pilkey',
+    id: 2
   },
   {
     image:
       'https://cdn.thebooksiogroup.com/cover_images/642/9781524798642.jpg?width=300&height=446&image-type=product_page_main_image&fit=bounds',
     title: 'Daisy Jones & The Six',
     author: 'Taylor Jenkins Reid',
+    id: 3
   },
   {
     image: 'https://m.media-amazon.com/images/I/91StpJ4e8gL.jpg',
     title: 'Hello Beautiful',
     author: 'Ann Napolitano',
+    id: 4
   },
 ];
 
@@ -31,9 +35,9 @@ function BookList() {
   return (
     <section className='booklist'>
       {books.map((book) => {
-        const {image, title, author} = book;
+        const {image, title, author, id} = book;
         return (
-            <Book image={image} title={title} author={author}/>
+            <Book image={image} title={title} author={author} key={id}/>
         );
       })}
     </section>
