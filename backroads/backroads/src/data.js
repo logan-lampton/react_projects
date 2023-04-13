8802bb6ea887bbc4b2ecaa9a1c236e6237b37e7a
Logan Lampton <lampton.logan@gmail.com>
2023-04-13 18:41:25 -0400

    refactored Services component to be dynamic

--- backroads/backroads/src/data.js
+++ backroads/backroads/src/data.js
@@ -39,20 +39,23 @@ export const socialLinks = [
   },
 ];
 
-// export const titleData = [
-//   {
-//     id: 1,
-//     wordOne: "About",
-//     wordTwo: "Us",
-//   },
-//   {
-//     id: 2,
-//     wordOne: "Our",
-//     wordTwo: "Services",
-//   },
-//   {
-//     id: 1,
-//     wordOne: "Featured",
-//     wordTwo: "Tours",
-//   },
-// ];
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, officia.",
+  },
+];
