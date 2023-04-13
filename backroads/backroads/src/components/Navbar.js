f1ed4ba02ebbb01e176141e040385b6a2e2ad571
Logan Lampton <lampton.logan@gmail.com>
2023-04-13 17:08:21 -0400

    refactored nav-links

--- backroads/backroads/src/components/Navbar.js
+++ backroads/backroads/src/components/Navbar.js
@@ -1,81 +1,64 @@
-import React from 'react'
-import logo from "../images/logo.svg"
+import React from "react";
+import logo from "../images/logo.svg";
+import { pageLinks } from "../data";
 
 const Navbar = () => {
   return (
     <>
-    <nav className='navbar'>
-          <div className='nav-center'>
-            <div className='nav-header'>
-              <img src={logo} className='nav-logo' alt='backroads' />
-              <button type='button' className='nav-toggle' id='nav-toggle'>
-                <i className='fas fa-bars'></i>
-              </button>
-            </div>
-
-            <ul className='nav-links' id='nav-links'>
-              <li>
-                <a href='#home' className='nav-link'>
-                  {" "}
-                  home{" "}
-                </a>
-              </li>
-
-              <li>
-                <a href='#about' className='nav-link'>
-                  {" "}
-                  about{" "}
-                </a>
-              </li>
-
-              <li>
-                <a href='#services' className='nav-link'>
-                  {" "}
-                  services{" "}
-                </a>
-              </li>
-
-              <li>
-                <a href='#tours' className='nav-link'>
-                  {" "}
-                  tours
-                </a>
-              </li>
-            </ul>
-
-            <ul className='nav-icons'>
-              <li>
-                <a
-                  href='https://www.twitter.com'
-                  target='_blank'
-                  className='nav-icon'
-                >
-                  <i className='fab fa-facebook'></i>
-                </a>
-              </li>
-              <li>
-                <a
-                  href='https://www.twitter.com'
-                  target='_blank'
-                  className='nav-icon'
-                >
-                  <i className='fab fa-twitter'></i>
-                </a>
-              </li>
-              <li>
-                <a
-                  href='https://www.twitter.com'
-                  target='_blank'
-                  className='nav-icon'
-                >
-                  <i className='fab fa-squarespace'></i>
-                </a>
-              </li>
-            </ul>
+      <nav className='navbar'>
+        <div className='nav-center'>
+          <div className='nav-header'>
+            <img src={logo} className='nav-logo' alt='backroads' />
+            <button type='button' className='nav-toggle' id='nav-toggle'>
+              <i className='fas fa-bars'></i>
+            </button>
           </div>
-        </nav>
-    </>
-  )
-}
 
-export default Navbar
+          <ul className='nav-links' id='nav-links'>
+            {pageLinks.map((link) => {
+              return (
+                <li key={link.id}>
+                  <a href={link.href} className='nav-link'>
+                    {link.text}
+                  </a>
+                </li>
+              );
+            })}
+          </ul>
+
+          <ul className='nav-icons'>
+            <li>
+              <a
+                href='https://www.twitter.com'
+                target='_blank'
+                className='nav-icon'
+              >
+                <i className='fab fa-facebook'></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://www.twitter.com'
+                target='_blank'
+                className='nav-icon'
+              >
+                <i className='fab fa-twitter'></i>
+              </a>
+            </li>
+            <li>
+              <a
+                href='https://www.twitter.com'
+                target='_blank'
+                className='nav-icon'
+              >
+                <i className='fab fa-squarespace'></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
