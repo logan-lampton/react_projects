19c7e2ad1773f6235abef150161594d07adee4af
Logan Lampton <lampton.logan@gmail.com>
2023-04-18 13:34:29 -0400

    added ability to clear the list

--- birthday-buddy/src/App.jsx
+++ birthday-buddy/src/App.jsx
@@ -1,17 +1,21 @@
 import { useState } from "react";
 import data from "./data";
-import List from './components/List';
-import Person from './components/Person';
+import List from "./components/List";
 
 function App() {
   const [people, setPeople] = useState(data);
 
+  const clearList = () => {
+    setPeople([]);
+  };
+
   return (
     <>
       <main>
-        <section className="container">
+        <section className='container'>
           <h3>There are {people.length} birthdays today:</h3>
-          <List people={people}/>
+          <List people={people} />
+          <button type='button' className='btn btn-block' onClick={clearList}>Clear List</button>
         </section>
       </main>
     </>
