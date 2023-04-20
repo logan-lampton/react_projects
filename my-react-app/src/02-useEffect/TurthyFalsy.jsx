149a98a45f7c567bb418b96592fe672b94d11bf5
Logan Lampton <lampton.logan@gmail.com>
2023-04-20 18:50:35 -0400

    work with ternary operator

--- my-react-app/src/02-useEffect/TurthyFalsy.jsx
+++ my-react-app/src/02-useEffect/TurthyFalsy.jsx
@@ -3,15 +3,24 @@ import { useState } from "react";
 function TurthyFalsy() {
   const [truthy, setTruthy] = useState(true);
   const [falsy, setFalsy] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
+  const codeExample = falsy || "show this text!";
 
   return (
-    <div>
-       <p>Falsy OR: {falsy || 'returns the first true value'}</p>
-       <p>Falsy AND: {falsy && 'returns the first false value'}</p>
-       <p>Truthy OR: {truthy || 'returns the first true value'}</p>
-       <p>Truthy AND: {truthy && 'returns the first false value'}</p>
-    </div>
-  )
+    <>
+      <div>
+        <p>Falsy OR: {falsy || "returns the first true value"}</p>
+        <p>Falsy AND: {falsy && "returns the first false value"}</p>
+        <p>Truthy OR: {truthy || "returns the first true value"}</p>
+        <p>Truthy AND: {truthy && "returns the first false value"}</p>
+        {codeExample}
+      </div>
+      <h2 style={{ margin: "1rem 0" }}>Ternary Operator</h2>
+      <button className='btn'>{isEditing ? "edit" : "add"}</button>
+      {truthy ? <div>Shows if true</div> : <div>Please login</div>}
+    </>
+  );
 }
 
 export default TurthyFalsy;
