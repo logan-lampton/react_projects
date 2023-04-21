148993a5db87f2f6946eb7ef0c6727a9ef1cb73c
Logan Lampton <lampton.logan@gmail.com>
2023-04-21 14:28:42 -0400

    clean up function with setInterval

--- my-react-app/src/02-useEffect/CleanUp.jsx
+++ my-react-app/src/02-useEffect/CleanUp.jsx
@@ -20,6 +20,15 @@ function CleanUp() {
 function SecondComponent() {
   useEffect(() => {
     console.log("renders each time the component loads");
+    const intId = setInterval(() => {
+      // keeps running even if the component is toggled off
+      console.log("timed message");
+    }, 1000);
+    // clean up function
+    // stops the interval when the component isn't displayed
+    return () => {
+      clearInterval(intId);
+    };
   }, []);
   return <div>Reset</div>;
 }
