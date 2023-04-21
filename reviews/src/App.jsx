3f6b142369311f1beae34e47dffe254df43f709f
Logan Lampton <lampton.logan@gmail.com>
2023-04-21 17:28:25 -0400

    added abilty to cycle through data using buttons

--- reviews/src/App.jsx
+++ reviews/src/App.jsx
@@ -7,9 +7,29 @@ const App = () => {
 
   const { id, name, job, image, text } = reviews[index];
 
+  const prevPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex - 1;
+      if (newIndex < 0) {
+        return reviews.length - 1;
+      }
+      return newIndex;
+    });
+  };
+
+  const nextPerson = () => {
+    setIndex((currentIndex) => {
+      const newIndex = currentIndex + 1;
+      if (newIndex > reviews.length - 1) {
+        return 0;
+      }
+      return newIndex;
+    });
+  };
+
   return (
     <main>
-      <article className='review' key={id}>
+      <article className='review'>
         <div className='img-container'>
           <img src={image} alt={name} className='person-img' />
           <span className='quote-icon'>
@@ -20,6 +40,14 @@ const App = () => {
         <h4 className='author'>{name}</h4>
         <p className='job'>{job}</p>
         <p className='info'>{text}</p>
+        <div className='btn-container'>
+          <button className='prev-btn' onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button className='next-btn' onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
       </article>
     </main>
   );
