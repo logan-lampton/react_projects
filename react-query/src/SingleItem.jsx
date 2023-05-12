e86629244d6ec82086102de7904cc6324e6e4c00
Logan Lampton <lampton.logan@gmail.com>
2023-05-12 14:27:28 -0400

    added ability to edit tasks

--- react-query/src/SingleItem.jsx
+++ react-query/src/SingleItem.jsx
@@ -1,15 +1,28 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import customFetch from "../server/utils";
+
 const SingleItem = ({ item }) => {
+  const queryClient = useQueryClient();
+
+  const { mutate: editTask } = useMutation({
+    mutationFn: ({ taskId, isDone }) => {
+      return customFetch.patch(`/${taskId}`, { isDone });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["tasks"] });
+    },
+  });
   return (
     <div className='single-item'>
       <input
         type='checkbox'
         checked={item.isDone}
-        onChange={() => console.log('edit task')}
+        onChange={() => editTask({ taskId: item.id, isDone: !item.isDone })}
       />
       <p
         style={{
-          textTransform: 'capitalize',
-          textDecoration: item.isDone && 'line-through',
+          textTransform: "capitalize",
+          textDecoration: item.isDone && "line-through",
         }}
       >
         {item.title}
@@ -17,7 +30,7 @@ const SingleItem = ({ item }) => {
       <button
         className='btn remove-btn'
         type='button'
-        onClick={() => console.log('delete task')}
+        onClick={() => console.log("delete task")}
       >
         delete
       </button>
