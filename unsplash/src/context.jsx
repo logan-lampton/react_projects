5bb4d881f822d7a924caa3a9052bd86d786b19a8
Logan Lampton <lampton.logan@gmail.com>
2023-05-13 23:55:47 -0400

    set up dark theme icon toggle via global context

--- unsplash/src/context.jsx
+++ unsplash/src/context.jsx
@@ -3,11 +3,14 @@ import { createContext, useContext, useState, useEffect } from "react";
 const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
-  const greeting = "hello";
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
 
-  return (
-    <AppContext.Provider value={{ greeting }}>{children}</AppContext.Provider>
-  );
+  const toggleDarkTheme = () => {
+    const newDarkTheme = !isDarkTheme;
+    setIsDarkTheme(newDarkTheme);
+  };
+
+  return <AppContext.Provider value={{isDarkTheme, toggleDarkTheme}}>{children}</AppContext.Provider>;
 };
 
 export const useGlobalContext = () => useContext(AppContext);
