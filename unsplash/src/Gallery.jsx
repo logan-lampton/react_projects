24f62e76fe1d2e7a586af5a07d0fd078caf269b7
Logan Lampton <lampton.logan@gmail.com>
2023-05-15 14:09:56 -0400

    images render on page; ReactQueryDevtools added

--- unsplash/src/Gallery.jsx
+++ unsplash/src/Gallery.jsx
@@ -1,19 +1,56 @@
 import axios from "axios";
-import { useQuery } from '@tanstack/react-query';
+import { useQuery } from "@tanstack/react-query";
 import unsplashKey from "/home/logan/Development/code/phase-6/private-key/unsplashKey.js";
 
 const url = `https://api.unsplash.com/search/photos/?client_id=${unsplashKey}&query=cat`;
 
 function Gallery() {
   const response = useQuery({
-    queryKey:['images'],
+    queryKey: ["images"],
     queryFn: async () => {
-      const resp = await axios.get(url)
-      return resp.data
-    }
-  })
-  console.log(response)
-  return(<h2>Gallery</h2>)
+      const resp = await axios.get(url);
+      return resp.data;
+    },
+  });
+  if (response.isLoading) {
+    return (
+      <section className='image-container'>
+        <h4>Loading...</h4>
+      </section>
+    );
+  }
+  if (response.isError) {
+    return (
+      <section className='image-container'>
+        <h4>There is an error, please re-enter your search</h4>
+      </section>
+    );
+  }
+  const results = response.data.results;
+
+  if (results.length < 1) {
+    return (
+      <section className='image-container'>
+        <h4>There are no results for your search term</h4>
+      </section>
+    );
+  }
+
+  return (
+    <section className='image-container'>
+      {results.map((image) => {
+        const url = image?.urls?.regular;
+        return (
+          <img
+            src={url}
+            key={image.id}
+            alt={image.alt_description}
+            className='img'
+          />
+        );
+      })}
+    </section>
+  );
 }
 
 export default Gallery;
