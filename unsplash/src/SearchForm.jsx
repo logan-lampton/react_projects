f9b7a7ba19f25f65e0449bf49cef3c0c90460c5c
Logan Lampton <lampton.logan@gmail.com>
2023-05-15 11:40:10 -0400

    set up image search form

--- unsplash/src/SearchForm.jsx
+++ unsplash/src/SearchForm.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+// Create a form with an input and a submit button. The input should have the following attributes: type='text', name='search', placeholder='cat', and className='form-input search-input'. When the user submits the form, access (for now log)the input value.
 
 function SearchForm() {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const searchValue = e.target.elements.search.value;
+    if (!searchValue) return;
+    console.log(searchValue);
+  };
+
   return (
-    <h2>SearchForm</h2>
-  )
+    <section>
+      <h1 className='title'>Unsplash Images</h1>
+      <form className='search-form' onSubmit={handleSubmit}>
+        <input
+          type='text'
+          name='search'
+          placeholder='cat'
+          className='form-input search-input'
+        ></input>
+        <button type='submit' className='btn'>
+          Search
+        </button>
+      </form>
+    </section>
+  );
 }
 
-export default SearchForm
+export default SearchForm;
