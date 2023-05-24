5d658964d9513dd67d2f60950a7037865ae9c812
Logan Lampton <lampton.logan@gmail.com>
2023-05-23 20:25:24 -0400

    set up logo/navbar

--- cocktails/src/components/Navbar.jsx
+++ cocktails/src/components/Navbar.jsx
@@ -1,5 +1,22 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
 
 export default function Navbar() {
-  return <h1>navbar component</h1>;
+  return (
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <Link to='/'>
+          <img src={logo} alt='Cocktail DB logo' className='logo' />
+        </Link>
+        <ul className='nav=links'>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about'>About</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
